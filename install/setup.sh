4345ea4b6ea2515f0da5806adf346bdb7bc6f3cd
grant <grant.wilkins371@gmail.com>
2020-08-31 13:58:03 -0400

    fast

--- install/setup.sh
+++ install/setup.sh
@@ -1,8 +1,8 @@
-COMPRESSOR_PATH = "/users/gfwilki/compressors"
+COMPRESSOR_PATH="/users/gfwilki/compressors"
 
 apt-get update
 apt-get install g++ cmake perl python3 libssl-dev git curl xz-utils tar gzip pkg-config bzip2 zstd patch snapd python-pip cpufrequtils linux-tools-common linux-tools-generic linux-cloud-tools-generic linux-tools-4.15.0-101-generic linux-cloud-tools-4.15.0-101-generic msr-tools
-snap install cmake
+snap install cmake --classic
 pip install numpy
 bash libpressio_install.sh
 
